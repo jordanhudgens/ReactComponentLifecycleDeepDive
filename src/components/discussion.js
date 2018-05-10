5a14fc1c6a36ce318c8b26e5f4ca57bbb4c20b00
jordanhudgens <jordan@devcamp.com>
2018-05-09 21:59:16 -0500

    Refactored discussion messages and discussion list out to separaate functional components

--- src/components/discussion.js
+++ src/components/discussion.js
@@ -1,11 +1,26 @@
 import React, { Component } from "react";
 
+const DiscussionMessage = props => {
+  return <div>{props.msg}</div>;
+};
+
+const DiscussionList = props => {
+  const allMessages = props.messages.map(message => {
+    return <DiscussionMessage msg={message} />;
+  });
+
+  return allMessages;
+};
+
 export default class Discussion extends Component {
   constructor() {
     super();
 
+    const messages = ["Some message", "Another message"];
+
     this.state = {
-      pageTitle: "Discussion"
+      pageTitle: "Discussion",
+      messages: messages
     };
   }
 
@@ -17,6 +32,11 @@ export default class Discussion extends Component {
   render() {
     const { pageTitle } = this.state;
 
-    return <h1>{pageTitle}</h1>;
+    return (
+      <div>
+        <h1>{pageTitle}</h1>
+        <DiscussionList messages={this.state.messages} />
+      </div>
+    );
   }
 }
